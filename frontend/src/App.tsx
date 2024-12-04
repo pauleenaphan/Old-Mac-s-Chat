bfa0123fd4fbe576d253da6739416ae35f7e1902
pauleenaphan <pauleena2002@gmail.com>
2024-12-04 14:08:33 -0800

    finished styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,14 +1,20 @@
-import { useState, useEffect, FormEvent } from 'react';
+import { useState, useEffect, FormEvent, useRef } from 'react';
 import './App.css';
 
 import io, { Socket } from 'socket.io-client';
+import { TbSend2 } from "react-icons/tb";
+import { GiFarmTractor } from "react-icons/gi";
+import { FaGithub } from "react-icons/fa";
+
+import farmerPfp from "./assets/farmerpfp.png";
 
 const SOCKET_URL = 'http://localhost:3001';
 // One page so we don't need routing
 function App() {
   const [socket, setSocket] = useState<Socket | null>(null);
   const [newMsg, setNewMsg] = useState<string>("");
-  const [messages, setMessages] = useState<{ content: string, date: string }[]>([]); // Array of messages
+  const [messages, setMessages] = useState<{ content: string, date: string }[]>([]);
+  const lastMessageRef = useRef<HTMLDivElement | null>(null);  // Array of messages
 
   useEffect(() => {
     // Fetch all messages from MongoDB using the new endpoint
@@ -41,8 +47,23 @@ function App() {
     };
   }, []);
 
+
+  useEffect(() => {
+    // Scroll to the last message whenever the messages array is updated
+    lastMessageRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages]);
+
   const handleSendMessage = (e: FormEvent) => {
     e.preventDefault();
+
+    // regex to check if input only has eio and spaces included
+    const isValid = /^[eioEIO\s]*$/.test(newMsg); 
+    
+    if (!isValid) {
+        alert("Message can only contain the letters E, I, or O.");
+        return;
+    }
+
     if (newMsg.trim() !== '' && socket) {
         // sends msg to server 
         socket.emit('message', newMsg);
@@ -52,35 +73,93 @@ function App() {
   };
 
   return (
-    <>
-      <header>
-        <h1> Old Mac's Chat! </h1>
-        <p> 
-          Send a message using only E-I-E-I-O. 
-          We are on Old MacDonald's farm! 
+    <div className='fullPageContainer'>
+      <div className="pageContainer">
+        <div className="lyricsContainer">
+        <p>
+          Old MacDonald had a farm <br />
+          Ee i ee i o <br />
+          And on his farm he had some cows <br />
+          Ee i ee i o <br />
+          With a moo-moo here <br />
+          And a moo-moo there <br />
+          Here a moo, there a moo <br />
+          Everywhere a moo-moo <br />
+          Old MacDonald had a farm <br />
+          Ee i ee i o <br />
+          Old MacDonald had a farm <br />
+          Ee i ee i o <br />
+          And on his farm he had some chicks <br />
+          Ee i ee i o <br />
+          With a cluck-cluck here <br />
+          And a cluck-cluck there <br />
+          Here a cluck, there a cluck <br />
+          Everywhere a cluck-cluck <br />
+          Old MacDonald had a farm <br />
+          Ee i ee i o <br />
+          Old MacDonald had a farm <br />
+          Ee i ee i o <br />
+          And on his farm he had some pigs <br />
+          Ee i ee i o <br />
+          With an oink-oink here <br />
+          And an oink-oink there <br />
+          Here an oink, there an oink <br />
+          Everywhere an oink-oink <br />
+          Old MacDonald had a farm <br />
+          Ee i ee i o <br />
         </p>
-      </header>
-      
-      <div className="allMsgContainer">
-          {messages.map((msg, index) => (
-            <div className="msgContainer" key={index}>
-                <div className="titleDateContainer">
-                  <h3> Anonymous Farmer </h3>
-                  <p> {msg.date} </p>
-                </div>
-                <p>
-                  {msg.content}
-                </p>
-
+        </div>
+        <div className="chatContainer">
+          <header>
+            <div className="titleContainer">
+              <h1> Old Mac's Global Chat! </h1>
+              <GiFarmTractor id="tractorIcon"/>
             </div>
-          ))}
+            <p> 
+              Send a message using only E-I-E-I-O. 
+              We are on Old MacDonald's farm! 
+            </p>
+          </header>
+          
+          <div className="allMsgContainer">
+              {messages.map((msg, index) => (
+                <div className="msgContainer" key={index}>
+                  <div className="pfpNameContainer">
+                    <img className="farmerPfp" src={farmerPfp} alt="farmerpfp"/>
+                    <div className="titleDateContainer">
+                      <h3> Anonymous Farmer </h3>
+                      <p> {msg.date} </p>
+                    </div>
+                  </div>
+                  <p>
+                    {msg.content}
+                  </p>
+                </div>
+              ))}
+              <div ref={lastMessageRef} />
+          </div>
+          
+          <form className="messageForm">
+            <textarea 
+              placeholder="Old McDonalds Only" 
+              value={newMsg} 
+              onChange={(e) =>{setNewMsg(e.target.value)}}
+              onKeyDown={(e) => {
+                if (e.key === "Enter" && !e.shiftKey) {
+                  e.preventDefault(); // Prevent newline
+                  handleSendMessage(e); // Trigger send message
+                }
+              }}
+            ></textarea>
+            <TbSend2 id="sendIcon" onClick={handleSendMessage}/>
+          </form>
+        </div>
       </div>
-      
-      <form className="messageForm" onSubmit={handleSendMessage}>
-        <textarea placeholder="Old McDonalds Only" value={newMsg} onChange={(e) =>{setNewMsg(e.target.value)}}></textarea>
-        <button> Send (make this send icon) </button>
-      </form>
-    </>
+      <footer>
+        @ 2024 Pauleena Phan. All rights reserved
+        <FaGithub />
+      </footer>
+    </div>
   )
 }
 
